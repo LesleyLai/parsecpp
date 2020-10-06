78f132a47e4e5c67aea6f55d3faed816c11ffc77
Lesley <lesleylai@lesleylai.info>
2020-10-06 13:35:47 -0600

    Integer parser

--- include/parsec.hpp
+++ include/parsec.hpp
@@ -3,29 +3,36 @@
 
 #include <optional>
 #include <string_view>
-#include <utility>
+#include <type_traits>
+
+#include <iostream>
 
 namespace parsec {
 
-template <class T>
-using ParseResult = std::optional<std::pair<T, std::string_view>>;
+template <class T> struct ParseOutput {
+  using Output = T;
+
+  Output output;
+  std::string_view remaining;
+};
+
+template <class T> using MaybeParseResult = std::optional<ParseOutput<T>>;
 
 // The type that the Parser tries to parse
 template <class Parser>
 using ParseType =
-    typename std::invoke_result_t<Parser,
-                                  std::string_view>::value_type::first_type;
+    typename std::invoke_result_t<Parser, std::string_view>::value_type::Output;
 
 struct CharParser {
   char c = 0;
   [[nodiscard]] constexpr auto operator()(std::string_view s) const
-      -> ParseResult<char>
+      -> MaybeParseResult<char>
   {
     if (s.empty() || s.front() != c) {
       return std::nullopt;
     }
     s.remove_prefix(1);
-    return std::make_pair(c, s);
+    return ParseOutput<char>{.output = c, .remaining = s};
   }
 };
 
@@ -39,18 +46,80 @@ struct CharParser {
   return CharParser{c};
 }
 
+/**
+ * @brief Creates a parser that parses an integer
+ */
+[[nodiscard]] constexpr auto is_digit(char c) noexcept -> bool
+{
+  return c >= '0' && c <= '9';
+}
+
+[[nodiscard]] constexpr auto to_digit(char c) noexcept -> int
+{
+  return c - '0';
+}
+
+[[nodiscard]] constexpr auto integer() noexcept
+{
+  return [](std::string_view s) -> MaybeParseResult<int> {
+    // TODO: Handle out of range
+    if (s.empty()) {
+      return std::nullopt;
+    }
+
+    // Parses the first character
+    int result = 0;
+    bool first_zero = false;
+    {
+      const char c = s[0];
+      if (!is_digit(c)) {
+        return std::nullopt;
+      } else if (c == '0') {
+        first_zero = true;
+      }
+      result = to_digit(c);
+    }
+
+    // Parses the second character
+    if (s.size() == 1 || !is_digit(s[1])) {
+      s.remove_prefix(1);
+      return ParseOutput<int>{.output = result, .remaining = s};
+    } else if (first_zero) { // Octal
+      return std::nullopt;
+    }
+    result = to_digit(s[1]) + result * 10;
+
+    // Parse the remaining characters
+    std::size_t i = 2;
+    for (; i < s.size(); ++i) {
+      const char c = s[i];
+      if (!is_digit(c)) {
+        break;
+      }
+      result = to_digit(c) + result * 10;
+    }
+
+    s.remove_prefix(i);
+    return ParseOutput<int>{.output = result, .remaining = s};
+  };
+}
+
 template <class Func, class Parser> struct MappedParser {
   Func func;
   Parser parser;
 
-  using Ret = ParseResult<std::invoke_result_t<Func, ParseType<Parser>>>;
-  [[nodiscard]] constexpr auto operator()(std::string_view s) const -> Ret
+  using Output = std::invoke_result_t<Func, ParseType<Parser>>;
+  using Ret = MaybeParseResult<Output>;
+  static constexpr bool is_noexcept =
+      std::is_nothrow_invocable_v<Func, ParseType<Parser>>;
+  [[nodiscard]] constexpr auto operator()(std::string_view s) const
+      noexcept(is_noexcept) -> Ret
   {
     const auto to_map = parser(s);
     if (!to_map) {
       return std::nullopt;
     }
-    return Ret(std::in_place, func(to_map->first), to_map->second);
+    return Ret({func(to_map->output), to_map->remaining});
   }
 };
 
--- test/parsec.cpp
+++ test/parsec.cpp
@@ -12,25 +12,77 @@ TEST_CASE("Matches a character")
 
   WHEN("can parse a string with matching front character")
   {
-    constexpr auto result_opt_const = parsec::character('H')("Hello, world!");
-    const auto result_opt = parsec::character('H')("Hello, world!");
+    constexpr auto parser = parsec::character('H');
 
     THEN("Generates that character and the tail of the string")
     {
-      STATIC_REQUIRE(result_opt_const);
-      STATIC_REQUIRE(result_opt_const->first == 'H');
-      STATIC_REQUIRE(result_opt_const->second == "ello, world!");
+      constexpr auto result_const = parser("Hello, world!");
+      STATIC_REQUIRE(result_const);
+      STATIC_REQUIRE(result_const->output == 'H');
+      STATIC_REQUIRE(result_const->remaining == "ello, world!");
 
-      REQUIRE(result_opt);
-      CHECK(result_opt->first == 'H');
-      CHECK(result_opt->second == "ello, world!");
+      const auto result = parser("Hello, world!");
+      REQUIRE(result);
+      CHECK(result->output == 'H');
+      CHECK(result->remaining == "ello, world!");
     }
   }
 
   THEN("Parsing a string with non-matching front gets no result")
   {
-    STATIC_REQUIRE(parsec::character('t')("Hello, world!") == std::nullopt);
-    REQUIRE(parsec::character('t')("Hello, world!") == std::nullopt);
+    constexpr auto parser = parsec::character('t');
+    STATIC_REQUIRE(parser("Hello, world!") == std::nullopt);
+    REQUIRE(parser("Hello, world!") == std::nullopt);
+  }
+}
+
+TEST_CASE("Int")
+{
+  GIVEN("An parser")
+  {
+    constexpr auto parser = parsec::integer();
+
+    THEN("Can parse 42")
+    {
+      STATIC_REQUIRE(parser("42"));
+      STATIC_REQUIRE(parser("42")->output == 42);
+      STATIC_REQUIRE(parser("42")->remaining == "");
+      REQUIRE(parser("42"));
+      REQUIRE(parser("42")->output == 42);
+      REQUIRE(parser("42")->remaining == "");
+    }
+
+    THEN("Can parse 1234567890")
+    {
+      STATIC_REQUIRE(parser("1234567890"));
+      STATIC_REQUIRE(parser("1234567890")->output == 1234567890);
+      STATIC_REQUIRE(parser("1234567890")->remaining == "");
+      REQUIRE(parser("1234567890"));
+      REQUIRE(parser("1234567890")->output == 1234567890);
+      REQUIRE(parser("1234567890")->remaining == "");
+    }
+
+    THEN("Negative numbers are not parsed")
+    {
+      STATIC_REQUIRE(!parser("-42"));
+      REQUIRE(!parser("-42"));
+    }
+
+    THEN("Can parse 0")
+    {
+      STATIC_REQUIRE(parser("0"));
+      STATIC_REQUIRE(parser("0")->output == 0);
+      STATIC_REQUIRE(parser("0")->remaining == "");
+      REQUIRE(parser("0"));
+      REQUIRE(parser("0")->output == 0);
+      REQUIRE(parser("0")->remaining == "");
+    }
+
+    THEN("Octal numbers are not parsed")
+    {
+      STATIC_REQUIRE(!parser("042"));
+      REQUIRE(!parser("042"));
+    }
   }
 }
 
@@ -50,9 +102,11 @@ TEST_CASE("Map")
     THEN("Gets correct output with good input")
     {
       STATIC_REQUIRE(parser("tea"));
-      STATIC_REQUIRE(parser("tea")->first == ('t' + 1));
+      STATIC_REQUIRE(parser("tea")->output == ('t' + 1));
+      STATIC_REQUIRE(parser("tea")->remaining == "ea");
       REQUIRE(parser("tea"));
-      REQUIRE(parser("tea")->first == ('t' + 1));
+      REQUIRE(parser("tea")->output == ('t' + 1));
+      REQUIRE(parser("tea")->remaining == "ea");
     }
   }
 }
