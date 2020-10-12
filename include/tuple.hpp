542f6fcb037380139ab0c68501a8dc923d331c9c
Lesley <lesleylai@lesleylai.info>
2020-10-12 00:17:11 -0600

    tuple apply

--- include/tuple.hpp
+++ include/tuple.hpp
@@ -1,23 +1,30 @@
 #ifndef PARSECPP_TUPLE_HPP
 #define PARSECPP_TUPLE_HPP
 
+#include <type_traits>
 #include <utility>
+
+#if _MSC_VER
+#define PARSEC_FORCE_INLINE __forceinline
+#else
+#define PARSEC_FORCE_INLINE inline __attribute__((always_inline))
+#endif
 
 namespace parsec {
 
 namespace detail {
 
-template <class... T> class TupleStorage;
+template <class... T> struct TupleStorage;
 
 template <> struct TupleStorage<> {
 };
 
 template <class Head, class... Tail>
-struct TupleStorage<Head, Tail...> : TupleStorage<Tail...> {
+struct TupleStorage<Head, Tail...> : public TupleStorage<Tail...> {
   Head value{};
 
   constexpr TupleStorage() = default;
-  explicit constexpr TupleStorage(Head&& head, Tail&&... tail) noexcept(
+  explicit PARSEC_FORCE_INLINE constexpr TupleStorage(Head&& head, Tail&&... tail) noexcept(
       noexcept(TupleStorage<Tail...>(std::forward<Tail>(tail)...)) && noexcept(
           Head{std::forward<Head>(head)}))
       : TupleStorage<Tail...>(std::forward<Tail>(tail)...),
@@ -26,7 +33,8 @@ struct TupleStorage<Head, Tail...> : TupleStorage<Tail...> {
   }
 
   template <std::size_t index>
-  [[nodiscard]] constexpr auto get() const noexcept -> const auto&
+  [[nodiscard]] PARSEC_FORCE_INLINE constexpr auto get() const noexcept -> const
+      auto&
   {
     if constexpr (index == 0) {
       return value;
@@ -38,20 +46,45 @@ struct TupleStorage<Head, Tail...> : TupleStorage<Tail...> {
 
 } // namespace detail
 
-template <class... T> struct Tuple : detail::TupleStorage<T...> {
+template <class... T> struct Tuple : public detail::TupleStorage<T...> {
   constexpr Tuple() noexcept = default;
 
-  explicit constexpr Tuple(T&&... values) noexcept(
+  explicit constexpr PARSEC_FORCE_INLINE Tuple(T&&... values) noexcept(
       noexcept(detail::TupleStorage<T...>(std::forward<T>(values)...)))
       : detail::TupleStorage<T...>(std::forward<T>(values)...)
   {
   }
 };
 
+template <class... T> Tuple(T&&...) -> Tuple<T...>;
+
 template <std::size_t index, class... T>
-[[nodiscard]] constexpr auto& get(const Tuple<T...>& t) noexcept
+[[nodiscard]] constexpr PARSEC_FORCE_INLINE auto
+get(const Tuple<T...>& t) noexcept -> const auto&
 {
   return t.template get<index>();
+}
+
+template <class Tuple> struct tuple_size;
+
+template <class... T>
+struct tuple_size<Tuple<T...>>
+    : std::integral_constant<std::size_t, sizeof...(T)> {
+};
+
+template <class Tuple>
+static constexpr std::size_t tuple_size_v = tuple_size<Tuple>::value;
+
+template <class F, class Tuple>
+constexpr auto PARSEC_FORCE_INLINE apply(F&& f, Tuple&& t) -> decltype(auto)
+{
+  return
+      [ f = std::forward<F>(f), t = std::forward<Tuple>(t) ]<std::size_t... I>(
+          std::index_sequence<I...>)
+  {
+    return std::invoke(f, parsec::get<I>(std::forward<Tuple>(t))...);
+  }
+  (std::make_index_sequence<tuple_size_v<std::remove_cvref_t<Tuple>>>{});
 }
 
 } // namespace parsec
--- test/tuple_test.cpp
+++ test/tuple_test.cpp
@@ -15,15 +15,43 @@ TEST_CASE("A default constructed tuple")
   REQUIRE(t.get<2>() == 0);
 }
 
+struct S {
+  int x;
+  int y;
+  double z;
+  constexpr friend auto operator==(S, S) -> bool = default;
+};
+
+constexpr auto to_s(int x, int y, double z)
+{
+  return S{.x = x, .y = y, .z = z};
+};
+
 TEST_CASE("A value constructed tuple")
 {
-  constexpr parsec::Tuple<int, int, int> t_const{1, 2, 3};
-  STATIC_REQUIRE(t_const.get<0>() == 1);
-  STATIC_REQUIRE(t_const.get<1>() == 2);
-  STATIC_REQUIRE(t_const.get<2>() == 3);
+  GIVEN("A tuple (1, 2, 3.5)")
+  {
+    constexpr parsec::Tuple t_const{1, 2, 3.5};
+    const parsec::Tuple t{1, 2, 3.5};
 
-  const parsec::Tuple<int, int, int> t{1, 2, 3};
-  REQUIRE(t.get<0>() == 1);
-  REQUIRE(t.get<1>() == 2);
-  REQUIRE(t.get<2>() == 3);
+    THEN("All of its elements has correct value")
+    {
+      STATIC_REQUIRE(t_const.get<0>() == 1);
+      STATIC_REQUIRE(t_const.get<1>() == 2);
+      STATIC_REQUIRE(t_const.get<2>() == 3.5);
+      REQUIRE(t.get<0>() == 1);
+      REQUIRE(t.get<1>() == 2);
+      REQUIRE(t.get<2>() == 3.5);
+    }
+
+    THEN("We can apply its elements with function")
+    {
+      constexpr auto expected = S{1, 2, 3.5};
+      constexpr S result_const = parsec::apply(to_s, t_const);
+      STATIC_REQUIRE(result_const == expected);
+
+      const S result = parsec::apply(to_s, t);
+      REQUIRE(result == expected);
+    }
+  }
 }
